1031a8fc272a91db4bf991def9cb830380c8a6ec
Kamil Jonaszko <kamil.jonaszko@wp.pl>
2018-08-28 10:18:08 +0200

    first commit

---
+++ Gra w życie/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Gra_w_życie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Animacja = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wczytajToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.koniecToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.animacjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Rysuj plansze";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 107);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 4;
+            this.panel1.Visible = false;
+            // 
+            // Animacja
+            // 
+            this.Animacja.Location = new System.Drawing.Point(211, 25);
+            this.Animacja.Name = "Animacja";
+            this.Animacja.Size = new System.Drawing.Size(75, 23);
+            this.Animacja.TabIndex = 5;
+            this.Animacja.Text = "Animacja";
+            this.Animacja.UseVisualStyleBackColor = true;
+            this.Animacja.Click += new System.EventHandler(this.Animacja_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.animacjaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(507, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zapiszToolStripMenuItem,
+            this.wczytajToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.koniecToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // zapiszToolStripMenuItem
+            // 
+            this.zapiszToolStripMenuItem.Name = "zapiszToolStripMenuItem";
+            this.zapiszToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.zapiszToolStripMenuItem.Text = "Zapisz";
+            this.zapiszToolStripMenuItem.Click += new System.EventHandler(this.zapiszToolStripMenuItem_Click);
+            // 
+            // wczytajToolStripMenuItem
+            // 
+            this.wczytajToolStripMenuItem.Name = "wczytajToolStripMenuItem";
+            this.wczytajToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.wczytajToolStripMenuItem.Text = "Wczytaj";
+            this.wczytajToolStripMenuItem.Click += new System.EventHandler(this.wczytajToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(112, 6);
+            // 
+            // koniecToolStripMenuItem
+            // 
+            this.koniecToolStripMenuItem.Name = "koniecToolStripMenuItem";
+            this.koniecToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.koniecToolStripMenuItem.Text = "Koniec";
+            this.koniecToolStripMenuItem.Click += new System.EventHandler(this.koniecToolStripMenuItem_Click);
+            // 
+            // animacjaToolStripMenuItem
+            // 
+            this.animacjaToolStripMenuItem.Name = "animacjaToolStripMenuItem";
+            this.animacjaToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.animacjaToolStripMenuItem.Text = "Animacja";
+            this.animacjaToolStripMenuItem.Click += new System.EventHandler(this.animacjaToolStripMenuItem_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10 x 10",
+            "15 x 10",
+            "15 x 15"});
+            this.comboBox1.Location = new System.Drawing.Point(43, 54);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 511);
+            this.Controls.Add(this.Animacja);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Animacja;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zapiszToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wczytajToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem koniecToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem animacjaToolStripMenuItem;
+    }
+}
+
---
+++ Gra w życie/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Gra_w_życie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            Animacja.Enabled = false;
+        }
+
+        Button[,] przycik;
+        Button[,] pom;
+        int x, y;
+        Random rand = new Random();
+        int pusty;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    przycik[i, j].Enabled = false;
+                    przycik[i, j].Visible = false;
+                    Array.Clear(przycik, i, j);
+                }
+            }
+            if (comboBox1.SelectedIndex == 0)
+            {
+                x = 10;
+                y = 10;
+            }   
+            if(comboBox1.SelectedIndex == 1)
+            {
+                x = 15;
+                y = 10;
+            }
+            if(comboBox1.SelectedIndex == 2)
+            {
+                x = 15;
+                y = 15;
+            }
+            przycik = new Button[x,y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    przycik[i, j] = new Button();
+                    przycik[i, j].Width = panel1.Width/x;
+                    przycik[i, j].Height = panel1.Height/x;
+                    przycik[i, j].BackColor = Button.DefaultBackColor;
+                    przycik[i, j].Left = (i * panel1.Width / x) + panel1.Left ;
+                    przycik[i, j].Top = (j * panel1.Height / x) + panel1.Top ;
+                    przycik[i, j].Click += new System.EventHandler(this.klik);
+                    przycik[i, j].Enabled = true;
+                    this.Controls.Add(przycik[i,j]);
+                }
+            }
+            Animacja.Enabled = true;
+        }
+
+        private void Animacja_Click(object sender, EventArgs e)
+        {
+            pom = new Button[x, y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    pom[i, j] = new Button();
+                    if (sprawdz_sasiadow(i, j) == 3 && przycik[i, j].BackColor == Button.DefaultBackColor)
+                    {
+                        pom[i, j].BackColor = Color.Blue;
+                    }
+                    else if (przycik[i, j].BackColor == Color.Blue && sprawdz_sasiadow(i, j) == 2 || sprawdz_sasiadow(i, j) == 3)
+                    {
+                        pom[i, j].BackColor = Color.Blue;
+                    }
+                    else if (przycik[i, j].BackColor == Color.Blue)
+                    {
+                        pom[i, j].BackColor = Button.DefaultBackColor;
+                    }
+                    this.Controls.Add(pom[i, j]);
+                }
+            }
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    przycik[i, j].BackColor = pom[i, j].BackColor;
+                    Array.Clear(pom, i, j);
+                }
+            }
+
+        }
+
+        private int sprawdz_sasiadow(int x, int y)
+        {
+            int licznik = 0;
+           
+                try
+                {
+                    if (przycik[x - 1, y + 1].BackColor == Color.Blue)
+                    {
+                        licznik++;
+                    }
+                }
+                catch
+                { }
+                try
+                {
+                    if (przycik[x, y + 1].BackColor == Color.Blue)
+                    {
+                        licznik++;
+                    }
+                }
+                catch
+                { }
+                try
+                {
+                    if (przycik[x + 1, y + 1].BackColor == Color.Blue)
+                    {
+                        licznik++;
+                    }
+                }
+                catch
+                { }
+                try
+                {
+                    if (przycik[x - 1, y].BackColor == Color.Blue)
+                    {
+                        licznik++;
+                    }
+                }
+                catch
+                { }
+                try
+                {
+                    if (przycik[x + 1, y].BackColor == Color.Blue)
+                    {
+                        licznik++;
+                    }
+                }
+                catch
+                { }
+                try
+                {
+                    if (przycik[x - 1, y - 1].BackColor == Color.Blue)
+                    {
+                        licznik++;
+                    }
+                }
+                catch
+                { }
+                try
+                {
+                    if (przycik[x, y - 1].BackColor == Color.Blue)
+                    {
+                        licznik++;
+                    }
+                }
+                catch
+                { }
+                try
+                {
+                    if (przycik[x + 1, y - 1].BackColor == Color.Blue)
+                    {
+                        licznik++;
+                    }
+                }
+                catch
+                { }
+            return licznik;
+        }
+
+        private void animacjaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    przycik[i, j].BackColor = Color.White;
+                }
+            }
+            Thread.Sleep(100);
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    pusty = rand.Next(0, 2);
+                    if (pusty == 1)
+                        przycik[i, j].BackColor = Color.Blue;
+                }
+            }
+        }
+
+        private void koniecToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void zapiszToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string line = "";
+            File.Delete(@"plansza.txt");
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    if(przycik[i, j].BackColor == Button.DefaultBackColor)
+                    {
+                        line += 0;
+                    }
+                    else
+                    {
+                        line += 1;
+                    }
+                }
+            }
+            File.WriteAllText(@"plansza.txt", line);
+        }
+
+        private void wczytajToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(@"plansza.txt"))
+            {
+                string line = "";
+                line = File.ReadAllText(@"plansza.txt");
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                        przycik[i, j].Enabled = false;
+                        przycik[i, j].Visible = false;
+                        Array.Clear(przycik, i, j);
+                    }
+                }
+                if (line.Length == 100)
+                {
+                    x = 10;
+                    y = 10;
+                }
+                if (line.Length == 150)
+                {
+                    x = 15;
+                    y = 10;
+                }
+                if (line.Length == 225)
+                {
+                    x = 15;
+                    y = 15;
+                }
+                int licznik = 0;
+                przycik = new Button[x, y];
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                        przycik[i, j] = new Button();
+                        przycik[i, j].Width = panel1.Width / x;
+                        przycik[i, j].Height = panel1.Height / x;
+                        if (line[licznik].ToString() == "1")
+                        {
+                            przycik[i, j].BackColor = Color.Blue;
+                        }
+                        else
+                        {
+                            przycik[i, j].BackColor = Button.DefaultBackColor;
+                        }
+                        przycik[i, j].Left = (i * panel1.Width / x) + panel1.Left;
+                        przycik[i, j].Top = (j * panel1.Height / x) + panel1.Top;
+                        przycik[i, j].Click += new System.EventHandler(this.klik);
+                        przycik[i, j].Enabled = true;
+                        licznik++;
+                        this.Controls.Add(przycik[i, j]);
+                    }
+                }
+                Animacja.Enabled = true;
+
+            }
+            else
+            {
+                MessageBox.Show("Blad pliku");
+            }
+        }
+
+        private void klik(object sender, EventArgs e)
+        {
+            Button przycisk = (sender as Button);
+            przycisk.BackColor = Color.Blue;
+        }
+    }
+}
